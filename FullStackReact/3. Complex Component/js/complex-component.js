7bab443a14cc82d7edd82815ade57bd7cb0f02b4
hvsharma <hvsharma_learning@outlook.com>
2017-11-02 18:52:42 +0530

    Complex Component + Restructuring

---
+++ FullStackReact/3. Complex Component/js/complex-component.js
@@ -0,0 +1,54 @@
+//Component example
+
+class App extends React.Component {
+    render() {
+       return (
+        <div className="notificationsFrame">
+            <div className="panel">
+                <Header />
+                <Content />
+            </div>
+        </div>
+       );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="menuIcon">
+                    <div className="dashTop"></div>
+                    <div className="dashBottom"></div>
+                    <div className="circle"></div>
+                </div>
+                <span className="title">Timeline</span>
+                <input type="text" className="searchInput" placeholder="Search..." />
+                <div className="fa fa-search searchIcon"></div>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <div className="line"></div>
+                { /* Timeline item */}
+                <div className="item">
+                    <div className="avatar">
+                        <img alt='Doug' src="http://www.croop.cl/UI/twitter/images/doug.jpg" />                        
+                    </div>
+                    <span className="time">An hour ago</span>
+                    <p>Ate lunch</p>
+                    <div className="commentCount">2</div>
+                    {/* ... */}
+                </div>
+            </div>
+        );
+    }
+}
+
+var mountRoot = document.querySelector("#app");
+ReactDOM.render(<App />, mountRoot);
